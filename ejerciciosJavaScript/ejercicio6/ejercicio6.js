2d30f05a94db9e1fb57049f503814bd455e8d66f
JorgeRmz <144190734+JorgeRmz16@users.noreply.github.com>
2023-11-09 14:40:54 -0600

    Update ejercicio6.js

--- ejerciciosJavaScript/ejercicio6/ejercicio6.js
+++ ejerciciosJavaScript/ejercicio6/ejercicio6.js
@@ -1,7 +1,15 @@
 //Algoritmo de burbuja.
 const nums1 = [3, 6, 12, 5, 100, 1 ];
-const numsSorted = nums1.sort(function(a,b){
-  return a - b;
-})
-console.log(`Array principal: ${nums1}`)
-console.log(`Array ordenado: ${numsSorted}`)
+function numSorted(lista){
+  for(let i = 0; i < lista.length -1; i++){
+    for(let j = 0; j < lista.length -i -1; j++){
+      if (lista[j] > lista[j + 1]){
+        var change = lista[j];
+        lista[j] = lista[j + 1];
+        lista[j + 1] = change;
+      }
+    }
+  }
+  return lista
+}
+numSorted(nums1)
